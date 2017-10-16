8bcee11898ee63a0863aa26453059e6a409f8272
Joseph Cutino <joseph.cutino@psware.com>
2017-10-16 13:53:48 -0400

    Initial attempt will likely not keep up with SPI speed. C-code needed

--- CY15B102Q.py
+++ CY15B102Q.py
@@ -18,7 +18,7 @@ except RuntimeError:
     print("Error importing RPi.GPIO")
 
 class CY15B102Q_SIM:
-    data = 0b00
+    data = 0x0
     opcodes = {'WREN':0x6,'READ': 0x3, 'WRITE': 0x2}
 
     #used to obtain opcode from input data
@@ -36,6 +36,9 @@ class CY15B102Q_SIM:
         self.MOSI = 13
         self.CS = 15
 
+        self.chipSelect = False
+        self.analyze = False
+
         #Create List for data storage
             #data stored in a dictionary to mimic address value lookup
         sim_RAM = {}
@@ -48,6 +51,14 @@ class CY15B102Q_SIM:
             #Wait for interrupt
             GPIO.wait_for_edge(self.CS, GPIO.RISING)
 
+    def sortDictionary(self,dic):
+        #sorts the data in the provided dictionary by key value (sim_memory address)
+        keylist = sorted(self.dic.iterkeys())
+        return keylist
+
+    def setSC(self):
+        self.chipSelect = False
+
     def SPI_slave_init(self):
         #Set up GPIO Pins
         GPIO.setmode(GPIO.BOARD)
@@ -55,7 +66,9 @@ class CY15B102Q_SIM:
         #Set up chip select with pull up as master should pull CS low during interaction
         GPIO.setup(self.CS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         #Add event to this pin so that when it is pull low the opcode is considered
-        GPIO.add_event_detect(self.CS, GPIO.FALLING, callback=self.chipSelected, bouncetime=50)
+        GPIO.add_event_detect(self.CS, GPIO.FALLING, callback=self.chipSelected)
+        #This interrupt will indicate when the master deselects this device
+        GPIO.add_event_detect(self.CS, GPIO.RISING, callback=self.setCS)
 
         #Set up MOSI (SI) as input
         GPIO.setup(self.MOSI, GPIO.IN)
@@ -65,27 +78,36 @@ class CY15B102Q_SIM:
 
         #Set up SCLK as input
         GPIO.setup(self.SCLK, GPIO.IN)
+        #Create event for clk signal. This will indicate when to read data.
+        GPIO.add_event_detect(self.SCLK, GPIO.RISING, callback=self.readMasterData)
 
     def chipSelected(self):
-        ##Establish SPI Mode based on first SCLK read
-            #This should actaully be unneed for these purposes because the following will read
-            #on the first rising edge regardless
-
-        #Create event for clk signal. This will indicate when to read data.
-        GPIO.add_event_detect(self.SCLK, GPIO.RISING, callback=self.readData, bouncetime=50)
+        #Used by other programs to order operations
+        self.chipSelect = True
 
         #active low
-        while not GPIO.input(self.CS):
-            #While chip select is held interupts will be triggered to read data from MOSI
+        while self.chipSelect:
+            #While chip select is held interupts will be triggered to start data read
+
+            #After all data has been read trigger analyze
+            if(self.analyze):
+                self.analyzeData()
             pass
 
-        #Remove action for now
-        GPIO.remove_event_detect(self.SCLK)
+        #After CS is released data register will be cleared
+        self.data = 0x0
+        #After CS is released all flags should be reset
+        self.analyze = False
 
-    def readData(self):
-        self.data << GPIO.input(self.MOSI)
+    def readMasterData(self):
+        if(self.chipSelect):
+            self.data << GPIO.input(self.MOSI)
+        if(bits == 23):
+            self.analyze == True
+            #Remove SCLK event so this input can be used for read/write events
+            GPIO.remove_event_detect(self.SCLK)
 
-    def analyzeData(self, data):
+    def analyzeData(self):
         #inspect first 8 bits for opcode
 
         ##WREN opcode sent after CS pulled low
@@ -98,19 +120,28 @@ class CY15B102Q_SIM:
                         start_address = ADDRESSMASK & self.data
                         self.sendData(start_address)
 
-
     def sendData(self,address):
         #Create event for clk signal. This will indicate when to write data.
         GPIO.add_event_detect(self.SCLK, GPIO.RISING)
-        while GPIO.input(self.CS):
+        #sorted list of keys
+        keylist = self.sortDictionary(sim_RAM)
+        while self.chipSelect:
             bdata=bin(sim_Ram[address])
             #If there is data left to send and clk is on the rising edge
-            while bdata and GPIO.event_detected(self.SCLK):
-                if(0x01 & bdata):
-                    GPIO.ouput(self.MISO, GPIO.HIGH)
-                else:
-                    GPIO.output(self.MISO, GPIO.LOW)
-                bdata = bdata >> 1
+            while bdata:
+                #if clk on rising edge send a bit of data
+                if GPIO.event_detected(self.SCLK):
+                    if(0x01 & bdata):
+                        GPIO.ouput(self.MISO, GPIO.HIGH)
+                    else:
+                        GPIO.output(self.MISO, GPIO.LOW)
+                    bdata = bdata >> 1
 
-            #Action is no longer required on rising clock cycles
-            GPIO.remove_event_detect(self.SCLK)
+            #increment address becasue HW should send data until master releases CS
+            newIndex = keylist.index(address)+1
+            if(newIndex >= len(keylist)):
+                newIndex = 0
+            address = keylist[newIndex]
+
+        #Action is no longer required on rising clock cycles
+        GPIO.remove_event_detect(self.SCLK)
---
+++ SPIMasterPi.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include "raspi/hw.h"
+#include "raspi/hw.c"
+
+#ifdef __XENO__
+#include <sys/mman.h>
+#include <rtdk.h>
+#define printf rt_printf
+#endif
+
+int main(int argc, char *argv[]){
+  uint32_t speed = ?????;
+  int destination =???;
+  uint8_t data = 0x15;
+
+  spi_init(speed);
+
+  spi_start(destination);
+
+  spi_write(data);
+
+  uint8_t rtn = spi_read();
+
+  if(spi_poll(1)){
+    uint8_t rtn = spi_read();
+    printf("%i",rtn)
+  }
+  spi_stop();
+}
