4091c12b2f4a4521fcf6e3ec501674a2b6b067c1
Joseph <cutinoj@mail.gvsu.edu>
2017-10-17 19:35:26 +0000

    pi attempt

--- ArduinoMasterCode/SPIMaster/SPIMaster.ino
+++ ArduinoMasterCode/SPIMaster/SPIMaster.ino
@@ -1,18 +1,19 @@
 #include <SPI.h>
 
 int CS = 10;
-int MOSI = 11;
+int DOUT = 11;
 int SCLK = 13;
 
 byte data = 0;
 
 void setup(){
-  SPI.begin()
+  SPI.begin();
   SPI.setBitOrder(MSBFIRST);
   pinMode(CS, OUTPUT);
-  pinMode(MOSI, OUTPUT);
+  pinMode(DOUT, OUTPUT);
   pinMode(SCLK, OUTPUT);
   
+  Serial.begin(9600);
   delay(500);
   
 }
@@ -22,6 +23,10 @@ void loop(){
   {
     digitalWrite(CS, LOW);
     SPI.transfer(data);
+    Serial.println(data);
+    digitalWrite(CS, HIGH);
+    digitalWrite(CS, LOW);
+    SPI.transfer(0x00);
     digitalWrite(CS, HIGH);
     delay(500);
   }
