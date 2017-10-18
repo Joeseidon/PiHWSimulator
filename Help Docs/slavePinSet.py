37298375dc39c79320a3c94d47731f5eb53329f6
Joseph <cutinoj@mail.gvsu.edu>
2017-10-18 13:48:12 -0400

    s

--- Help Docs/slavePinSet.py
+++ Help Docs/slavePinSet.py
@@ -1,10 +1,14 @@
 import pigpio
 
 def main():
+    pi = pigpio.pi()
+    if not pi.connected:
+        print("not connected")
+        exit()
     pi.set_mode(2, pigpio.INPUT)
     pi.set_mode(2, pigpio.INPUT)
     pi.set_mode(14, pigpio.INPUT)
-
+    print("Pins set")
     while True:
         pass
 
