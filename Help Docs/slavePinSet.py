e7ecb17865b60416c0ade9244d1cd3ca2f94b2a4
Joseph <cutinoj@mail.gvsu.edu>
2017-10-18 13:47:05 -0400

    test

---
+++ Help Docs/slavePinSet.py
@@ -0,0 +1,12 @@
+import pigpio
+
+def main():
+    pi.set_mode(2, pigpio.INPUT)
+    pi.set_mode(2, pigpio.INPUT)
+    pi.set_mode(14, pigpio.INPUT)
+
+    while True:
+        pass
+
+if __name__ == "__main__":
+    main()
