618e059d0b80c7ba1c166b950731cd448d1aa553
Joseph <cutinoj@mail.gvsu.edu>
2017-10-18 13:34:30 -0400

    mod

--- Help Docs/piTest.py
+++ Help Docs/piTest.py
@@ -10,6 +10,8 @@ def main():
     pi.set_mode(9, pigpio.INPUT)
     pi.set_mode(4, pigpio.OUTPUT)
     pi.set_mode(24, pigpio.OUTPUT)
+    pi.set_mode(11, pigpio.OUTPUT)
+    pi.set_mode(8, pigpio.OUTPUT)
 
     flash_500=[]
     flash_500.append(pigpio.pulse(1<<4, 1<<24, 500000))
@@ -20,15 +22,20 @@ def main():
     f500 = pi.wave_create()
 
     pi.wave_send_repeat(f500)
-    
-    h = pi.spi_open(0,32000,3)
+
+    '''h = pi.spi_open(0,32000,3)
     while True:
         pi.spi_write(h,[2, 192, 128])
         (b,d)=pi.spi_read(h,3)
         if b==3:
             print(d)
         else:
-            print("No data")
+            print("No data")'''
+    while True:
+        (count, rx_data) = pi.spi_xfer(h, [1,128,29])
+        print(count)
+        print(rx_data)
+        time.sleep(4)
 
 if __name__ == "__main__":
     main()
